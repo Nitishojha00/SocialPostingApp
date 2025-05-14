cccde3450989738c044a9cb83bf083357fffdd4e
Nitishojha00 <nitishojha00@gmail.com>
2025-05-15 00:48:05 +0530

    updated database

--- db.js
+++ db.js
@@ -1,12 +1,21 @@
-require('dotenv').config()
+// require('dotenv').config()
+// const mongoose = require('mongoose');
+// // const DATABASE_URI = process.env.DATABASE_LOCAL;
+// const DATABASE_URI = process.env.DATABASE_URI;
+
+// const Database = mongoose.connect(DATABASE_URI).then(()=>{
+//     console.log("Database Connected");
+// }).catch((err)=>{
+//     console.log("Error to connect Database",err);
+// })
+
 const mongoose = require('mongoose');
-// const DATABASE_URI = process.env.DATABASE_LOCAL;
-const DATABASE_URI = process.env.DATABASE_URI;
 
-const Database = mongoose.connect(DATABASE_URI).then(()=>{
-    console.log("Database Connected");
-}).catch((err)=>{
-    console.log("Error to connect Database",err);
-})
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.error("❌ MongoDB connection error:", err));
 
-module.exports = Database
+// If you want to export the connection
+module.exports = mongoose;
